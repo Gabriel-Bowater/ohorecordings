737347c437633ccda9e718143129e7af54586e6e
Gabriel-Bowater <gd.bowater@gmail.com>
2015-09-23 12:01:45 +1200

    move cart item count logic to ApplicationController. Run cart_item_count before each render. Throw alert windows when user trys to play sample when sample does not exist.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :user_cart_count
+
   def index
   	@user = User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def user_cart_count
+  	@user_cart_count = 0
+  	if Order.where(user_id: session[:user_id], completed: false)[0]
+  	  @user_cart_count += AlbumOrder.where(order_id: Order.where(user_id: session[:user_id], completed: false)[0].id).length
+  	  @user_cart_count +=TrackOrder.where(order_id: Order.where(user_id: session[:user_id], completed: false)[0].id).length
+    end
   end
 end
